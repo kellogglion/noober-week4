bd38607d81f6baa1831e91468e5fe7360d1c400c
kellogglion <puskoor@kelloggalumni.northwestern.edu>
2021-04-29 11:56:18 -0500

    commit

--- noober.js
+++ noober.js
@@ -42,13 +42,99 @@ window.addEventListener('DOMContentLoaded', async function() {
 
     if(Purple == true){
       Car = 'Noober Purple'
+      // Create a variable for the the rides class we're going to add to
+
+      let ridesElement = document.querySelector(`.rides`)
+
+      // Insert HTML into each rides element using the data from json
+  
+      ridesElement.insertAdjacentHTML(`beforeend`, `
+      
+      
+      <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
+        <i class="fas fa-car-side"></i>
+        <span>${Car}</span>
+      </h1>
+  
+      <div class="border-8 border-purple-900 p-4 my-4 text-left">
+  
+  
+        <div class="flex">
+          <div class="w-1/2">
+            <h2 class="text-2xl py-1">${FirstName} ${LastName}</h2>
+            <p class="font-bold text-gray-600">${Phone}</p>
+          </div>
+          <div class="w-1/2 text-right">
+            <span class="rounded-xl bg-gray-600 text-white p-2">
+              ${NumberPassengers}
+            </span>
+          </div>
+        </div>
+        <div class="mt-4 flex">
+          <div class="w-1/2">
+            <div class="text-sm font-bold text-gray-600">PICKUP</div>
+            <p>${PickAddress}</p>
+            <p>${PickCity}, ${PickState} ${PickZip}</p>
+          </div>
+          <div class="w-1/2">
+            <div class="text-sm font-bold text-gray-600">DROPOFF</div>
+            <p>${DropAddress}</p>
+            <p>${DropCity}, ${DropState} ${DropZip}</p>
+          </div>
+        </div>
+      </div> 
+      
+      
+      `)
     }else if(NumberPassengers > 3){
       Car = 'Noober XL'
+          // Create a variable for the the rides class we're going to add to
+
+    let ridesElement = document.querySelector(`.rides`)
+
+    // Insert HTML into each rides element using the data from json
+
+    ridesElement.insertAdjacentHTML(`beforeend`, `
+    
+    
+    <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-4xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
+      <i class="fas fa-car-side"></i>
+      <span>${Car}</span>
+    </h1>
+
+    <div class="border-4 border-gray-900 p-4 my-4 text-left">
+
+
+      <div class="flex">
+        <div class="w-1/2">
+          <h2 class="text-4xl font-bold py-1">${FirstName} ${LastName}</h2>
+          <p class="font-bold text-gray-600">${Phone}</p>
+        </div>
+        <div class="w-1/2 text-right">
+          <span class="rounded-xl bg-gray-600 text-white p-2">
+            ${NumberPassengers}
+          </span>
+        </div>
+      </div>
+      <div class="mt-4 flex">
+        <div class="w-1/2">
+          <div class="text-3x1 font-bold text-gray-600">PICKUP</div>
+          <p>${PickAddress}</p>
+          <p>${PickCity}, ${PickState} ${PickZip}</p>
+        </div>
+        <div class="w-1/2">
+          <div class="text-3x1 font-bold text-gray-600">DROPOFF</div>
+          <p>${DropAddress}</p>
+          <p>${DropCity}, ${DropState} ${DropZip}</p>
+        </div>
+      </div>
+    </div> 
+    
+    
+    `)
     }else{
       Car = 'Noober X'
-    }
-
-    // Create a variable for the the rides class we're going to add to
+          // Create a variable for the the rides class we're going to add to
 
     let ridesElement = document.querySelector(`.rides`)
 
@@ -63,6 +149,8 @@ window.addEventListener('DOMContentLoaded', async function() {
     </h1>
 
     <div class="border-4 border-gray-900 p-4 my-4 text-left">
+
+
       <div class="flex">
         <div class="w-1/2">
           <h2 class="text-2xl py-1">${FirstName} ${LastName}</h2>
@@ -90,6 +178,9 @@ window.addEventListener('DOMContentLoaded', async function() {
     
     
     `)
+    }
+
+    
   
   
   
